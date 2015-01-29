85fe748cd984efef51be703f3d669a61c61e982a
Achu Abebe <achusime@gmail.com>
2015-01-29 20:13:19 +0300

    Fix `librarian-puppet` command not being found

--- puppet/manifests/bootstrap.pp
+++ puppet/manifests/bootstrap.pp
@@ -7,6 +7,11 @@ import "./core/*", "./bootstrap/*"
 #
 class bootstrap
 {
+Exec {
+    path        => "/bin/:/sbin/:/usr/bin/:/usr/sbin/:/usr/local/bin",
+    path        => "/bin/:/sbin/:/usr/bin/:/usr/sbin/:/usr/local/bin:/opt/vagrant_ruby/bin/",
+     environment => "HOME=/home/vagrant"
+   }
   include core::params
 
   include bootstrap::update,
